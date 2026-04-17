e71fa0715b07d100ecfb785d608b740edfee3117
Marius <subrinmarius@gmail.com>
2026-04-17 11:09:52 +0200

    Feat Android.MainViewModel

---
+++ android/app/src/main/java/fr/studio/voxel/organ/domain/model/SidebarViewModel.kt
@@ -0,0 +1,21 @@
+package fr.studio.voxel.organ.domain.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class SidebarViewModel : ViewModel() {
+
+    var selectedProjectId by mutableStateOf<Int?>(null)
+    var selectedOrganId by mutableStateOf<Int?>(null)
+
+    fun selectProject(id: Int) {
+        selectedProjectId = id
+        selectedOrganId = null
+    }
+
+    fun selectOrgan(id: Int) {
+        selectedOrganId = id
+    }
+}
