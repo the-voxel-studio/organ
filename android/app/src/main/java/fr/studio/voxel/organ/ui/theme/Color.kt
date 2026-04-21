62fcc259678e68ce53327814a44c38e76ec66c60
Marius <subrinmarius@gmail.com>
2026-04-21 11:06:37 +0200

    Feat Android.color And Android.font

--- android/app/src/main/java/fr/studio/voxel/organ/ui/theme/Color.kt
+++ android/app/src/main/java/fr/studio/voxel/organ/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package fr.studio.voxel.organ.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPink = Color(0xFFFF89D8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PastelPink = Color(0xFFFF7DD4)
+
+val DarkBlue = Color(0xFF355EE4)
+
+val LightBlue = Color(0xFF45A7D2)
+
+val PastelBlue = Color(0xFF45A7D2)
+
+val White = Color(0xFFFFF6F6)
+
+val Black = Color(0xFF0D0630)
