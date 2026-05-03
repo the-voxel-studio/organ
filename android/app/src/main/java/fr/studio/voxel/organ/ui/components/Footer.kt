c10d7d17bf09e39c6c609a0248a9f94071189b1c
Marius <subrinmarius@gmail.com>
2026-05-03 11:53:08 +0200

    Fix And.Theme Add AppColorScheme and AppTypography

--- android/app/src/main/java/fr/studio/voxel/organ/ui/components/Footer.kt
+++ android/app/src/main/java/fr/studio/voxel/organ/ui/components/Footer.kt
@@ -1,73 +1,46 @@
 package fr.studio.voxel.organ.ui.components
 
-
-import android.os.Build.VERSION.SDK_INT
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.ImageLoader
-import coil.compose.AsyncImage
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
-import coil.request.ImageRequest
 import fr.studio.voxel.organ.R
+import fr.studio.voxel.organ.ui.theme.AppColorScheme
+import fr.studio.voxel.organ.ui.theme.AppTypography
+
 @Composable
 fun Footer(
     modifier: Modifier = Modifier
 ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxWidth()
     ) {
         Row(
-            modifier = modifier
-                .padding(20.dp),
+            modifier = modifier.padding(20.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = modifier
-                .padding(20.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButtonPressable(
+            IconButtonPressable(
                 icon = R.drawable.icon_account,
                 contentDescription = "Account",
                 onClick = { /* TODO */ }
-                )
+            )
 
-                Spacer(modifier = Modifier.width(24.dp))
+            Spacer(modifier = Modifier.width(24.dp))
 
-                Text(
-                    text = "Nom Prénom",
-                    style = MaterialTheme.typography.labelLarge,
-                    color = MaterialTheme.colorScheme.onSurface,
-                )
-            }
+            Text(
+                text = "Nom Prénom",
+                style = AppTypography.labelLarge,
+                color = AppColorScheme.onSurface,
+            )
 
             Spacer(modifier = Modifier.width(24.dp))
 
--- android/app/src/main/java/fr/studio/voxel/organ/ui/components/Header.kt
+++ android/app/src/main/java/fr/studio/voxel/organ/ui/components/Header.kt
@@ -1,16 +1,12 @@
 package fr.studio.voxel.organ.ui.components
 
-import android.os.Build.VERSION.SDK_INT
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,25 +14,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.ImageLoader
-import coil.compose.AsyncImage
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
-import coil.request.ImageRequest
 import fr.studio.voxel.organ.R
-import kotlin.Boolean
+import fr.studio.voxel.organ.ui.theme.AppColorScheme
+import fr.studio.voxel.organ.ui.theme.AppTypography
 
 @Composable
 fun Header(
@@ -45,34 +31,36 @@ fun Header(
     isSideBarClosed: Boolean = false,
     navigateUp: () -> Unit
 ) {
-
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 20.dp)
     ) {
         if (canOpenSidebar) {
-        IconButton(
-            onClick = navigateUp,
-            modifier = Modifier.align(Alignment.CenterStart)
-        ) {
-            Icon(Icons.Default.Menu, contentDescription = "Menu")
+            IconButton(
+                onClick = navigateUp,
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu",
+                    tint = AppColorScheme.onSurface
+                )
+            }
         }
-    }
         Row(
             modifier = Modifier.align(Alignment.Center),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
-
             AnimatedLogo()
 
             Spacer(modifier = Modifier.width(8.dp))
 
             Text(
                 text = "Organ",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.onSurface,
+                style = AppTypography.titleLarge,
+                color = AppColorScheme.onSurface,
             )
         }
 
@@ -84,21 +72,19 @@ fun Header(
                     painter = painterResource(R.drawable.poubelle_logo),
                     contentDescription = "poubelle",
                     modifier = Modifier
-                            .size(24.dp)
+                        .size(24.dp)
                         .clickable {
-                            {/*TODO*/}
+                            /*TODO*/
                         }
                 )
-
                 Spacer(modifier = Modifier.width(24.dp))
-
                 Image(
                     painter = painterResource(R.drawable.notification_logo),
                     contentDescription = "notification",
                     modifier = Modifier
                         .size(24.dp)
                         .clickable {
-                            {/*TODO*/}
+                            /*TODO*/
                         }
                 )
             }
